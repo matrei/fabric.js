0c8fca1d4e5f922a9f00f2d823480098ef0cd4e0
Stefan Kienzle <stefankienzle.de@gmail.com>
2014-07-08 13:32:58 +0200

    Revert JSCS tweaks for single-line return statements.

--- src/canvas.class.js
+++ src/canvas.class.js
@@ -339,9 +339,7 @@
      * @param {fabric.Object} target
      */
     _shouldCenterTransform: function (e, target) {
-      if (!target) {
-        return;
-      }
+      if (!target) return;
 
       var t = this._currentTransform,
           centerTransform;
@@ -405,9 +403,7 @@
      * @param {fabric.Object} target
      */
     _setupCurrentTransform: function (e, target) {
-      if (!target) {
-        return;
-      }
+      if (!target) return;
 
       var pointer = this.getPointer(e),
           corner = target._findTargetCorner(this.getPointer(e, true)),
@@ -477,9 +473,7 @@
           lockScalingY = target.get('lockScalingY'),
           lockScalingFlip = target.get('lockScalingFlip');
 
-      if (lockScalingX && lockScalingY) {
-        return;
-      }
+      if (lockScalingX && lockScalingY) return;
 
       // Get the constraint point
       var constraintPosition = target.translateToOriginPoint(target.getCenterPoint(), t.originX, t.originY),
@@ -634,9 +628,7 @@
 
       var t = this._currentTransform;
 
-      if (t.target.get('lockRotation')) {
-        return;
-      }
+      if (t.target.get('lockRotation')) return;
 
       var lastAngle = atan2(t.ey - t.top, t.ex - t.left),
           curAngle = atan2(y - t.top, x - t.left),
@@ -735,9 +727,7 @@
      * @param {Boolean} skipGroup when true, group is skipped and only objects are traversed through
      */
     findTarget: function (e, skipGroup) {
-      if (this.skipTargetFind) {
-        return;
-      }
+      if (this.skipTargetFind) return;
 
       if (this._isLastRenderedObject(e)) {
         return this.lastRenderedObjectWithControlsAboveOverlay;
@@ -1117,9 +1107,7 @@
      */
     _drawObjectsControls: function(ctx) {
       for (var i = 0, len = this._objects.length; i < len; ++i) {
-        if (!this._objects[i] || !this._objects[i].active) {
-          continue;
-        }
+        if (!this._objects[i] || !this._objects[i].active) continue;
         this._objects[i]._renderControls(ctx);
         this.lastRenderedObjectWithControlsAboveOverlay = this._objects[i];
       }
